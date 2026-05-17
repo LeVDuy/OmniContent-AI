a7c6c336c9c7e4f0b8dc4b05c8e3fd44e7a6312e
LeVDuy <levanduyhd98@gmail.com>
2026-05-18 01:57:07 +0200

    fix: add sys.path

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,8 @@
 import pytest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi.testclient import TestClient
 from Fast_API import app
 
